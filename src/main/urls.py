9622e38f9461228910ff981af6759cc16ff1736a
Saqib <127623510+Abdul-Saqib@users.noreply.github.com>
2024-05-06 03:52:29 -0400

    Delete src directory

--- src/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import main_view, predict, result # Import the predict view function
-urlpatterns = [
-    path('', main_view, name='main'),   
-    path('predict/', predict, name='predict'),
-    path('predict/result/', result, name='result'),
-]
